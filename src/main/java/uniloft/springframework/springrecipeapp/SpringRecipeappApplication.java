559e15cd776cdc8ca8276c172f93a7f945376a95
uniloftsky <uniloftsky@gmail.com>
2020-09-16 15:04:13 +0300

    Project added

---
+++ src/main/java/uniloft/springframework/springrecipeapp/SpringRecipeappApplication.java
@@ -0,0 +1,13 @@
+package uniloft.springframework.springrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipeappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uniloft/springframework/springrecipeapp/SpringRecipeappApplicationTests.java
@@ -0,0 +1,13 @@
+package uniloft.springframework.springrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
