166976f1803c2a086f2621f3f9f0583d8cfc6119
uniloftsky <uniloftsky@gmail.com>
2020-09-20 00:57:02 +0300

    Query testing via spring boot features

--- src/main/java/uniloft/springframework/springrecipeapp/controllers/IndexController.java
+++ src/main/java/uniloft/springframework/springrecipeapp/controllers/IndexController.java
@@ -2,13 +2,30 @@ package uniloft.springframework.springrecipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import uniloft.springframework.springrecipeapp.model.Category;
+import uniloft.springframework.springrecipeapp.model.UnitOfMeasure;
+import uniloft.springframework.springrecipeapp.repositories.CategoryRepository;
+import uniloft.springframework.springrecipeapp.repositories.UofRepository;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UofRepository uofRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UofRepository uofRepository) {
+        this.categoryRepository = categoryRepository;
+        this.uofRepository = uofRepository;
+    }
+
     @RequestMapping({"/", "", "index", "index.html", "privet"})
     public String getIndexPage() {
-        System.out.println("Some to say...123433322 ssss 111 222");
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = uofRepository.findByUof("Teaspoon");
+        System.out.println("Category id is: " + categoryOptional.get().getId());
+        System.out.println("Uof id is: " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 
--- src/main/java/uniloft/springframework/springrecipeapp/repositories/UofRepository.java
+++ src/main/java/uniloft/springframework/springrecipeapp/repositories/UofRepository.java
@@ -3,5 +3,10 @@ package uniloft.springframework.springrecipeapp.repositories;
 import org.springframework.data.repository.CrudRepository;
 import uniloft.springframework.springrecipeapp.model.UnitOfMeasure;
 
+import java.util.Optional;
+
 public interface UofRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByUof(String uof);
+
 }
