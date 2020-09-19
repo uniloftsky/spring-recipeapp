b3ccc781a31b4cb13e42c577005709758c4e0c47
uniloftsky <uniloftsky@gmail.com>
2020-09-20 00:05:30 +0300

    Created Category JPA Entity and Many to Many relationship with recipes

--- src/main/java/uniloft/springframework/springrecipeapp/model/Recipe.java
+++ src/main/java/uniloft/springframework/springrecipeapp/model/Recipe.java
@@ -27,6 +27,10 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
@@ -124,5 +128,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
