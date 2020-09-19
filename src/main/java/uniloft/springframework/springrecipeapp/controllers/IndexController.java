f78b6067d1f3760bb862e4afc30d09d5a75589d7
uniloftsky <uniloftsky@gmail.com>
2020-09-19 22:52:51 +0300

    Created JPA model for Recipe and Notes entities

--- src/main/java/uniloft/springframework/springrecipeapp/controllers/IndexController.java
+++ src/main/java/uniloft/springframework/springrecipeapp/controllers/IndexController.java
@@ -1,14 +1,14 @@
 package uniloft.springframework.springrecipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping({"/", "", "index", "index.html"})
-    public String getIndexPage(Model model) {
+    @RequestMapping({"/", "", "index", "index.html", "privet"})
+    public String getIndexPage() {
+        System.out.println("Some to say...123433322 ssss 111 222");
         return "index";
     }
 
