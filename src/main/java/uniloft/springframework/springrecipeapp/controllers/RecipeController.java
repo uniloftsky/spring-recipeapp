75941419a4614a28ce45143c59567fc7cce7d709
unilofsky <uniloftsky@gmail.com>
2020-09-30 14:48:15 +0300

    Created recipe updating

--- src/main/java/uniloft/springframework/springrecipeapp/controllers/RecipeController.java
+++ src/main/java/uniloft/springframework/springrecipeapp/controllers/RecipeController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import uniloft.springframework.springrecipeapp.commands.RecipeCommand;
 import uniloft.springframework.springrecipeapp.model.Recipe;
 import uniloft.springframework.springrecipeapp.services.RecipeService;
 
@@ -19,7 +18,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id_recipe}")
+    @RequestMapping("/recipe/{id_recipe}/show")
     public String showById(@PathVariable String id_recipe, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id_recipe)));
         return "recipe/show";
@@ -27,7 +26,13 @@ public class RecipeController {
 
     @RequestMapping("recipe/new")
     public String newRecipe(Model model) {
-        model.addAttribute("recipe", new RecipeCommand());
+        model.addAttribute("recipe", new Recipe());
+        return "recipe/recipeform";
+    }
+
+    @RequestMapping("recipe/{id_recipe}/update")
+    public String updateRecipe(@PathVariable String id_recipe, Model model) {
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id_recipe)));
         return "recipe/recipeform";
     }
 
@@ -35,7 +40,7 @@ public class RecipeController {
     @RequestMapping("recipe")
     public String saveOrUpdate(@ModelAttribute Recipe recipe) {
         Recipe savedRecipe = recipeService.saveRecipe(recipe);
-        return "redirect:/recipe/show/" + savedRecipe.getId();
+        return "redirect:/recipe/" + savedRecipe.getId() + "/show";
     }
 
 }
